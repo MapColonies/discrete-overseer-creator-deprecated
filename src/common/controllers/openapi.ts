502f7f0663a820ec9bc4d7be61956a650f6ad3fc
shahar s <cl_shaharshavit@rnd-hub.com>
2021-01-07 16:37:01 +0200

    feat: added relative ref support to openapi

--- src/common/controllers/openapi.ts
+++ src/common/controllers/openapi.ts
@@ -1,27 +1,76 @@
 import { readFileSync } from 'fs';
 import * as openapiUi from 'swagger-ui-express';
-import { Request, Response, RequestHandler } from 'express';
+import { Request, Response, RequestHandler, NextFunction } from 'express';
 import { safeLoad } from 'js-yaml';
 import { injectable, inject } from 'tsyringe';
 import { IConfig } from 'config';
+import { resolveRefs } from 'json-refs';
+import httpStatus from 'http-status-codes';
 import { Services } from '../constants';
 import { ILogger, OpenApiConfig } from '../interfaces';
 @injectable()
 export class OpenapiController {
   public uiMiddleware: RequestHandler[];
-  public serveUi: RequestHandler;
-
-  private readonly openapiDoc: openapiUi.JsonObject;
+  
+  private uiHandler?: RequestHandler;
+  private openapiDoc?: openapiUi.JsonObject;
 
   public constructor(@inject(Services.LOGGER) private readonly logger: ILogger, @inject(Services.CONFIG) private readonly config: IConfig) {
     const openapiConfig = config.get<OpenApiConfig>('openapiConfig');
-
-    this.openapiDoc = safeLoad(readFileSync(openapiConfig.filePath, 'utf8')) as openapiUi.JsonObject;
-    this.serveUi = openapiUi.setup(this.openapiDoc);
     this.uiMiddleware = openapiUi.serve;
+    this.init(openapiConfig).catch(err=>{throw err});
+  }
+
+  public serveUi(req: Request, res: Response, next: NextFunction): void {
+    if (this.uiHandler) {
+      this.uiHandler(req, res, next);
+    } else {
+      res.sendStatus(httpStatus.SERVICE_UNAVAILABLE);
+    }
   }
 
   public serveJson(req: Request, res: Response): void {
     res.json(this.openapiDoc);
+  }
+
+  private async init(openapiConfig: OpenApiConfig): Promise<void> {
+    const openapiDefinition = readFileSync(openapiConfig.filePath, 'utf8');
+    const parsedDefinition = safeLoad(openapiDefinition) as openapiUi.JsonObject;
+    this.openapiDoc = (await this.multiFileSwagger(
+      parsedDefinition
+    ).catch((err) => {
+      throw err;
+    })) as openapiUi.JsonObject;
+    this.uiHandler = openapiUi.setup(this.openapiDoc, {
+      swaggerOptions: { basePath: './Schema' },
+    });
+  }
+
+  private async multiFileSwagger(
+    root: Record<string, unknown>
+  ): Promise<void | Record<string, unknown>> {
+    const options = {
+      filter: ['relative', 'remote'],
+      location: './docs/openapi3.yaml',
+      loaderOptions: {
+        processContent: function (
+          res: { text: string },
+          callback: unknown
+        ): void {
+          const cb = callback as (a: null, b: unknown) => void;
+          cb(null, JSON.parse(res.text));
+        },
+      },
+    };
+
+    const logger = this.logger;
+    return resolveRefs(root, options).then(
+      function (results) {
+        return results.resolved as Record<string, unknown>;
+      },
+      function (err: Error) {
+        logger.log('warn',`failed to load swagger reference: ${err.message}.`);
+      }
+    );
   }
 }
