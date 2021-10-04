cb544befa6633b94caf98135ecc11089ad2731ce
Anguiar <shacharroy14@gmail.com>
2021-10-04 16:10:06 +0300

    chore: add tests

--- tests/mocks/config.ts
+++ tests/mocks/config.ts
@@ -1,11 +1,30 @@
+import config from 'config';
 import { IConfig } from '../../src/common/interfaces';
 
+let mockConfig: Record<string, unknown> = {};
 const getMock = jest.fn();
 const hasMock = jest.fn();
 
-const configMock = ({
+const configMock = {
   get: getMock,
   has: hasMock,
-} as unknown) as IConfig;
+} as IConfig;
 
-export { getMock, hasMock, configMock };
+const init = (): void => {
+  getMock.mockImplementation((key: string): unknown => {
+    return mockConfig[key] ?? config.get(key);
+  });
+};
+
+const setValue = (key: string | Record<string, unknown>, value?: unknown): void => {
+  if (typeof key === 'string') {
+    mockConfig[key] = value;
+  } else {
+    mockConfig = {...mockConfig, ...key };
+  }
+};
+
+const clear = (): void => {
+  mockConfig = {};
+};
+export { getMock, configMock, setValue, clear, init };
---
+++ tests/mocks/filesManager.ts
@@ -0,0 +1,8 @@
+import { FilesManager } from '../../src/utils/filesManager';
+const readFileSyncMock = jest.fn();
+
+const filesManagerMock = {
+  readFileSync: readFileSyncMock,
+} as unknown as FilesManager;
+
+export { filesManagerMock };
